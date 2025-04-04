d9da004b5107c6d483850c8d84e8ce9814653840
Dilshod <dilshod.normurodov1392@gmail.com>
2025-04-05 06:21:09 +0900

    updated footer

--- app/_components/footer/Footer.jsx
+++ app/_components/footer/Footer.jsx
@@ -9,14 +9,14 @@ import { Logo } from '../header/logo/Logo';
 
 const Footer = () => {
   return (
-    <div className='bg-white md:pt-5 pt-5 md:pb-5 md:mt-10 mt-5 border-t-2 border-slate-200 pb-5 md:sticky md:bottom-0 z-50'>
+    <div className='bg-white md:pt-5 pt-5 md:pb-5 md:mt-10 mt-5 border-t-2 border-slate-200 pb-5'>
       <section className='flex md:flex-row flex-col md:space-x-4 space-y-3 md:space-y-0 justify-between md:w-4/5 mx-auto text-center items-center '>
         <Logo />
-        <Link href='/' className='md:text-2xl text-lg font-bold text-slate-600'><InstagramIcon /> Instagram</Link>
-        <Link href='https://t.me/Edu_Vix' className='md:text-2xl text-lg font-bold text-slate-600'><TelegramIcon /> Telegram</Link>
-        <Link href='/' className='md:text-2xl text-lg font-bold text-slate-600'><YouTubeIcon /> YouTube</Link>
-        <Link href='/' className='md:text-2xl text-lg font-bold text-slate-600'><LinkedInIcon /> LinkedIn</Link>
-        <Link href='/' className='md:text-2xl text-lg font-bold text-slate-600 md:mb-2 mb-2'>© 2025,  <span className='text-indigo-500'>EduVix</span> </Link>
+        <Link href='/' className='md:text-xl text-lg font-bold text-slate-600'><InstagramIcon /> Instagram</Link>
+        <Link href='https://t.me/Edu_Vix' className='md:text-xl text-lg font-bold text-slate-600'><TelegramIcon /> Telegram</Link>
+        <Link href='/' className='md:text-xl text-lg font-bold text-slate-600'><YouTubeIcon /> YouTube</Link>
+        <Link href='/' className='md:text-xl text-lg font-bold text-slate-600'><LinkedInIcon /> LinkedIn</Link>
+        <Link href='/' className='md:text-xl text-lg font-bold text-slate-600 md:mb-2 mb-2'>© 2025,  <span className='text-indigo-500'>EduVix</span> </Link>
       </section>
     </div>
   )
